0eda44c732850ac212b7d51b2252c2a06df3e695
Pedro Elias <elias.pedrocontato@gmail.com>
2026-02-24 18:55:22 +0000

    declaração das variaveis

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -6,8 +6,12 @@
 
 int main() {
   // Área para definição das variáveis para armazenar as propriedades das cidades
-
+  char estadoCarta1, estadoCarta2;
+  char codigoCarta1[4], codigoCarta2[4], nomeCarta1[50], nomeCarta2[50];
+  int populacaoCarta1, populacaoCarta2, pontoTuristicoCarta1, pontoTuristicoCarta2;
+  float areaCarta1, areaCarta2, pibCarta1, pibCarta2;
   // Área para entrada de dados
+
 
   // Área para exibição dos dados da cidade
 
