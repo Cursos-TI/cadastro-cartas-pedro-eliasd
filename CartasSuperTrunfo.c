77ae78ba5b4ccdeb87b3d4f4b2aa8bdc607f5ff8
Pedro Elias <elias.pedrocontato@gmail.com>
2026-02-24 19:30:04 +0000

    entrada de dados carta 2

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -12,6 +12,8 @@ int main() {
   float areaCarta1, areaCarta2, pibCarta1, pibCarta2;
   // Área para entrada de dados
   printf("Olá seja bem vindo ao cadastro de cartas do Super Trunfo\nVamos começar cadastrando os dados da Carta 1\n");
+
+  //carta 1
   printf("Digite o estado da carta 1(Letra de A a H):\n");
   scanf(" %c", &estadoCarta1);
   printf("Digite o código da carta 1(Letra do estado + numero de 01 a 04):\n");
@@ -27,7 +29,25 @@ int main() {
   printf("Digite o número de pontos turisticos da cidade:\n");
   scanf("%d", &pontoTuristicoCarta1);
 
+  //Carta 2
+  printf("Agora vamos cadastrar a segunda carta:\n");
+  printf("Digite o estado da carta 2(Letra de A a H):\n");
+  scanf(" %c", &estadoCarta2);
+  printf("Digite o código da carta 2(Letra do estado + numero de 01 a 04):\n");
+  scanf("%s", codigoCarta2);
+  printf("Digite o nome da cidade:\n");
+  scanf(" %[^\n]", nomeCarta2);
+  printf("Digite o número de habitantes da cidade:\n");
+  scanf("%d", &populacaoCarta2);
+  printf("Digite a área da cidade (km²):\n");
+  scanf("%f", &areaCarta2);
+  printf("Digite o PIB da cidade:\n");
+  scanf("%f", &pibCarta2);
+  printf("Digite o número de pontos turisticos da cidade:\n");
+  scanf("%d", &pontoTuristicoCarta2);
+
   // Área para exibição dos dados da cidade
+  printf("Cartas Cadastradas:\n");
 
 return 0;
 } 
